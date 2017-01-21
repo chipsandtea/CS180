4ee04706761579f7561e94813beec829be446f4c
Michael James Gates <mjgates@unix1.lt.ucsc.edu>
2017-01-20 16:08:18 -0800

    Lab1 all requirements met, begin testing

--- lab1/create.sql
+++ lab1/create.sql
@@ -11,13 +11,44 @@ CREATE SCHEMA Lab1;
 -- Create tables
 
 CREATE TABLE Persons (
-    SSN INT,
+    SSN INTEGER PRIMARY KEY,
     NAME CHAR(30),
-    HouseID INT,
-    ApartmentNumber INT,
+    HouseID INTEGER,
+    ApartmentNumber INTEGER,
     -- Decimal with at most 5 digits to the left of the decimal point
     --    and 2 decimal spaces after it.
     Salary DECIMAL(7,2)
 );
 
+CREATE TABLE Houses (
+    HouseID INTEGER PRIMARY KEY,
+    HouseAddress VARCHAR(40),
+    ApartmentCount INTEGER,
+    Color VARCHAR(40)
+);
 
+CREATE TABLE Landlords (
+    LandlordID INTEGER PRIMARY KEY,
+    OwnerSSN INTEGER,
+    LandlordAddress VARCHAR(40)
+);
+
+CREATE TABLE Ownerships (
+    LandlordID INTEGER,
+    HouseID INTEGER,
+    PurchaseDate DATE,
+    PropertyTax DECIMAL(7,2),
+    PRIMARY KEY (LandlordID, HouseID)
+);
+
+CREATE TABLE Tenants (
+    HouseID INTEGER,
+    ApartmentNumber INTEGER,
+    LeaseTenantSSN INTEGER,
+    LeaseStartDate DATE,
+    LeaseExpirationDate DATE,
+    Rent DECIMAL(7,2),
+    LastRentPaidDate DATE,
+    RentOverdue BOOLEAN,
+    PRIMARY KEY (HouseID, ApartmentNumber)
+);
