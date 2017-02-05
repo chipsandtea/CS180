9cf5d18951f40852d14fc453dfeb81b2a81193ee
Michael Gates <mjgates@ucsc.edu>
2017-02-04 18:37:23 -0800

    query 4 testing

--- lab2/query4.sql
+++ lab2/query4.sql
@@ -2,3 +2,6 @@
 -- Find the names of all persons who lease an apartment whose rent is higher than half of that person's salary.
 
 
+SELECT Persons.Name
+FROM Persons, Tenants
+WHERE Persons.Salary/2 > Tenants.Rent AND Tenants.LeaseTenantSSN = Persons.SSN;
