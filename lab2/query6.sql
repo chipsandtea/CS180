32885917561f90049408c72153194b20bca271f1
Michael Gates <mjgates@ucsc.edu>
2017-02-05 18:37:43 -0800

    Working on query 6

---
+++ lab2/query6.sql
@@ -0,0 +1,4 @@
+-- Lab2, Query 6
+-- For the tenancies that have an overdue rent, find the address and apartment number of the tenancy, the ssn, name and salary of the lease tenant, and the name of the landlord of the leased apartment. THe attribute for the lease tenant's name should appear as TenantName, and the attribute for the landlord's name should appear as LandlordName.
+
+SELECT
