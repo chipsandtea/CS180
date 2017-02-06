07975dac9e7ee1bd49f7e7cc641a6d9dce158a2c
Michael Gates <mjgates@ucsc.edu>
2017-02-05 19:18:14 -0800

    Working on query6 with subqueries

--- lab2/query6.sql
+++ lab2/query6.sql
@@ -1,4 +1,9 @@
 -- Lab2, Query 6
 -- For the tenancies that have an overdue rent, find the address and apartment number of the tenancy, the ssn, name and salary of the lease tenant, and the name of the landlord of the leased apartment. THe attribute for the lease tenant's name should appear as TenantName, and the attribute for the landlord's name should appear as LandlordName.
-
-SELECT
+-- ApartmentCount, SSN, p.Name as TenantName, Salary, p.Name as LandlordName
+SELECT h.HouseAddress
+FROM Houses h
+WHERE h.HouseID IN (SELECT t.HouseID
+                FROM Tenants t
+                WHERE t.RentOverdue = true)
+;
